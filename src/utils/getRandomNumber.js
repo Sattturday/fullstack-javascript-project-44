7d817aef52a5d8db54afc31c095ce9ec20d0f080
Alla Magina <babara@flylady.su>
2025-12-05 14:59:43 +0300

    Add comment explaining Math.random usage

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,3 +1,4 @@
+// Math.random() is acceptable here because cryptographic security is not required
 export const getRandomNumber = (max) => {
   const randomNumber = Math.floor(Math.random() * max)
 
